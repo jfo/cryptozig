1d478d06efb7dbd672afca9f5753e71f78f961e4
Jeff Fowler <jeffowler@gmail.com>
2017-11-11 15:33:24 +0100

    reorg utils

--- main.zig
+++ main.zig
@@ -1,25 +1,13 @@
-const warn = @import("std").debug.warn;
 const assert = @import("std").debug.assert;
-const encode = @import("std").base64.encode;
 const mem = @import("std").mem;
 
-fn hexDigit(c: u8) -> u8 {
-    switch (c) {
-        '0'...'9' => c - '0',
-        'a'...'f' => c - 'a' + 10,
-        'A'...'F' => c - 'A' + 10,
-        else => u8(@maxValue(u8))
-    }
-}
+const utils = @import("./utils.zig");
+const base64 = @import("std").base64;
 
-fn hexDigits(dest: []u8, src: []const u8) -> []u8 {
-    var i:u32 = 0;
-    while (i < src.len) {
-        dest[i / 2] = hexDigit(src[i]) << 4 | hexDigit(src[i + 1]);
-        i+=2;
-    }
-    return dest[0..src.len / 2 ];
-}
+const hexDigits = utils.hexDigits;
+const one_char_xor = utils.one_char_xor;
+const scorer = utils.scorer;
+const fixed_xor = utils.fixed_xor;
 
 test "Convert hex to base64" {
     const src = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
@@ -30,15 +18,10 @@ test "Convert hex to base64" {
     const output_raw = hexDigits(dest[0..], src[0..]);
 
     var dest2:[src.len]u8 = undefined;
-    const output_base64 = encode(dest2[0..], output_raw[0..output_raw.len]);
+    const output_base64 = base64.encode(dest2[0..], output_raw[0..output_raw.len]);
 
     assert(mem.eql(u8, expected_output_raw, output_raw));
     assert(mem.eql(u8, expected_output_base64, output_base64));
-}
-
-fn fixed_xor(dest: []u8, src: []const u8, src2: []const u8) -> []u8 {
-    for (src) |c, i| dest[i] = c ^ src2[i];
-    dest[0..]
 }
 
 test "Fixed XOR" {
@@ -63,22 +46,6 @@ test "Fixed XOR" {
     assert(mem.eql(u8, expected_output_raw, out));
 }
 
-fn one_char_xor(dest: []u8, src: []const u8, c:u8) -> []u8 {
-    for (src) |char, i| dest[i] = char ^ c;
-    dest[0..]
-}
-
-// this naive score function simply counts spaces
-fn scorer(src: []const u8) -> u8 {
-    var count:u8 = 0;
-    for (src) |char| {
-        if (char == 32) {
-            count += 1
-        }
-    }
-    count
-}
-
 test "Single-byte XOR cipher" {
     const src = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
     const expected_output = "Cooking MC's like a pound of bacon";
@@ -101,32 +68,5 @@ test "Single-byte XOR cipher" {
     assert(mem.eql(u8, expected_output, winner));
 }
 
-const stdio = @cImport(@cInclude("stdio.h"));
-
 test "Detect single-character XOR" {
-    // const x = stdio.printf("djif");
-
-    // const src = "0e3647e8592d35514a081243582536ed3de6734059001e3f535ce6271032";
-    // const expected_output = "Cooking MC's like a pound of bacon";
-
-    // var hexed:[src.len / 2]u8 = undefined;
-    // const src_raw = hexDigits(hexed[0..], src[0..]);
-
-    // var i:u8 = 0;
-    // // var winner:[src.len / 2]u8 = undefined;
-    // var dest:[src.len / 2]u8 = undefined;
-
-    // while (i < @maxValue(u8)) {
-    //     var out_xor = one_char_xor(dest[0..], src_raw, i);
-    //     i+=1;
-    //     // warn("{}", dest);
-    //     // if (scorer(dest) > scorer(winner)) {
-    //     //     mem.copy(u8, winner[0..], dest[0..]);
-    //     // }
-    // }
-
-    // assert(mem.eql(u8, expected_output, winner));
 }
-
-
-
--- utils.zig
+++ utils.zig
@@ -1,6 +1,41 @@
+const warn = @import("std").debug.warn;
+const mem = @import("std").mem;
+
 // none of this is in any way done or intended beyond my own debuggery use.
 
-const warn = @import("std").debug.warn;
+pub fn hexDigit(c: u8) -> u8 {
+    switch (c) {
+        '0'...'9' => c - '0',
+        'a'...'f' => c - 'a' + 10,
+        'A'...'F' => c - 'A' + 10,
+        else => u8(@maxValue(u8))
+    }
+}
+
+pub fn hexDigits(dest: []u8, src: []const u8) -> []u8 {
+    var i:u32 = 0;
+    while (i < src.len) {
+        dest[i / 2] = hexDigit(src[i]) << 4 | hexDigit(src[i + 1]);
+        i+=2;
+    }
+    return dest[0..src.len / 2 ];
+}
+
+pub fn one_char_xor(dest: []u8, src: []const u8, c:u8) -> []u8 {
+    for (src) |char, i| dest[i] = char ^ c;
+    dest[0..]
+}
+
+// this naive score function simply counts spaces
+pub fn scorer(src: []const u8) -> u8 {
+    var count:u8 = 0;
+    for (src) |char| {
+        if (char == 32) {
+            count += 1
+        }
+    }
+    count
+}
 
 pub fn printLn(line: []u8) -> void {
     for (line) |c| warn("{c}", c);
@@ -15,4 +50,9 @@ pub fn readlines(dest: [][]u8, content: []u8) -> [][]u8 {
             idx2 = i + 1;
     };
     dest
+}
+
+pub fn fixed_xor(dest: []u8, src: []const u8, src2: []const u8) -> []u8 {
+    for (src) |c, i| dest[i] = c ^ src2[i];
+    dest[0..]
 }
